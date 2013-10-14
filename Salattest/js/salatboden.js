3738e7986fce81c47874414063aaa7f8c98e5c3a
matsls09 <mats-liver.skandsen@hotmail.com>
2013-10-14 15:22:58 +0200

    forstørr/forminsk

--- Salattest/js/salatboden.js
+++ Salattest/js/salatboden.js
@@ -5,3 +5,10 @@ obj = document.getElementById(objId);
 currentSize = parseFloat(obj.style.fontSize); //parseFloat gives you just the numerical value, i.e. strips the 'em' bit away
 obj.style.fontSize = (currentSize + .1) + "em";
 }
+
+function decreaseFontSize(objId) {
+obj = document.getElementById(objId);
+//get current font size of obj
+currentSize = parseFloat(obj.style.fontSize); //parseFloat gives you just the numerical value, i.e. strips the 'em' bit away
+obj.style.fontSize = (currentSize - .1) - "em";
+}
