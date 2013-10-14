cae82aeb9cc244ea35fbc54632783e2ca48edd07
Ola Malnes <ola.malnes@gmail.com>
2013-10-14 13:13:27 +0200

    Salatboden
    Alle filer så langt

---
+++ Salattest/js/salatboden.js
@@ -0,0 +1,7 @@
+
+function increaseFontSize(objId) {
+obj = document.getElementById(objId);
+//get current font size of obj
+currentSize = parseFloat(obj.style.fontSize); //parseFloat gives you just the numerical value, i.e. strips the 'em' bit away
+obj.style.fontSize = (currentSize + .1) + "em";
+}
