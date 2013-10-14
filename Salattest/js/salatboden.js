ee21f359c2549f49190f533b79fcebbba094211a
matsls09 <mats-liver.skandsen@hotmail.com>
2013-10-14 15:36:11 +0200

    forminsk fortsettelse

--- Salattest/js/salatboden.js
+++ Salattest/js/salatboden.js
@@ -10,5 +10,5 @@ function decreaseFontSize(objId) {
 obj = document.getElementById(objId);
 //get current font size of obj
 currentSize = parseFloat(obj.style.fontSize); //parseFloat gives you just the numerical value, i.e. strips the 'em' bit away
-obj.style.fontSize = (currentSize - .1) - "em";
+obj.style.fontSize = (currentSize - .1) + "em";
 }
