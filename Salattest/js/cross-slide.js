084b2da5a621b8e5e5e41714e04908784d15c41f
Ola Malnes <ola.malnes@gmail.com>
2013-10-21 12:48:11 +0200

    litt forskjellig

--- Salattest/js/cross-slide.js
+++ Salattest/js/cross-slide.js
@@ -1,1 +1,135 @@
-uagent = window.navigator.userAgent.toLowerCase();IEB=(uagent.indexOf('msie') != -1)?true:false;OPB=(uagent.indexOf('opera') != -1)?true:false;CRB=(uagent.indexOf('chrome') != -1)?true:false;SFB=(uagent.indexOf('safari') != -1)?true:false;FFB=(uagent.indexOf('firefox') != -1)?true:false;var scompat = document.compatMode;function secondcomplate(){this.c=1;}function slide1ScSlideShow(){var CursorStr="default";var Xpos=0;var yy=0;function fffffffffffff(){if((0==1)&&(0==0)){fff1(ll);fff1f(llll);fffffffff(llll);}f1ffffffffffff(llll);}function f1fffffffffff(){ll1lllllllllll=spageObj.appendChild(document.createElement("IMG"));ll1lllllllllll.src="";ll1lllllllllll.style.position="absolute";ll1lllllllllll.style.zIndex=0+7;ll1lllllllllll.style.visibility="inherit";ll1lllllllllll.style.left=0+"px";ll1lllllllllll.style.top=0+"px";if(0==1){ll1lllllllllll.style.width=""+0+"px";ll1lllllllllll.style.height=""+0+"px";}if(100==100){return;}if(IEB==true){if(100!=100){ll1lllllllllll.style.filter="progid:DXImageTransform.Microsoft.Alpha(opacity="+100+")";}}else{ll1lllllllllll.style.opacity=0.100;}}function f1ffffffffffff(gcur){if(0!=1){return;}ll1llllllllllll.innerHTML=""+""+(gcur+1)+"&nbsp;of&nbsp;"+3+"";}function f1fffffffffffff(){if(0!=1){return;}ll1llllllllllll = spageObj.appendChild(document.createElement("DIV"));ll1llllllllllll.style.position="absolute";ll1llllllllllll.style.zIndex=0+6;ll1llllllllllll.style.display="block";ll1llllllllllll.style.left=""+0+"px";ll1llllllllllll.style.top=""+0+"px";ll1llllllllllll.style.borderWidth=""+0+"px";ll1llllllllllll.style.borderStyle="solid";    ll1llllllllllll.style.visibility="inherit";ll1llllllllllll.style.textAlign="left";ll1llllllllllll.style.cursor='default';ll1llllllllllll.style.overflow="visible";ll1llllllllllll.style.fontFamily="Verdana";ll1llllllllllll.style.fontSize=10+"pt";    ll1llllllllllll.style.fontStyle="normal";ll1llllllllllll.style.fontWeight="bold";    ll1llllllllllll.style.color="#000000";ll1llllllllllll.innerHTML="";}function ff1fff(){ix=this.indx;if(linkstr[ix].substring(0,11)=="javascript:"){eval(""+linkstr[ix]);return;}if(targetstr[ix]==''){targetstr[ix]='_self';}if(IEB){window.open(''+linkstr[ix],''+targetstr[ix]);}else{if((targetstr[ix].indexOf("_parent")>-1)){eval("parent.window.location='"+linkstr[ix]+"'");}else if((targetstr[ix].indexOf("_top")>-1)){eval("top.window.location='"+linkstr[ix]+"'");}else{window.open(''+linkstr[ix],''+targetstr[ix]);}}}function f1fffff(){lll=ll-1;if(lll<0){lll=3-1;}}function f1ff(){if((0==1)&&(thumbmouseic==1)){return;}Beklemedemi=1;llll=lll;if(0==1){ffffffffffffff();}fffffffffffff();Sonraki();var ekbekleme=0;if(IEB){ekbekleme=parseInt(1000*1.0);}else{ekbekleme=0;}if(llllllll==1){if(!((ReachedTheEnd==1)&&(1==0))){if(tam==0){if(ll1lllllll==0){devam("f1ffff",(ekbekleme+5000));}}else{if(ll1lllllll==0){ll1ll=setTimeout('slide1.f1ffff()',(ekbekleme+5000));}} }else{ll1ll=setTimeout("slide1.ff1ffff()",5000);}}}function f1fff(){op=op-dif;op=(Math.floor(op*100))/100;if(op<(0.01)){op=0.00;}if(op>(0.99)){op=1.00;}lllllllllllll[ll].style.opacity = op;lllllllllllll[lll].style.opacity = 1.00-op;if(op>(0.00)){ll1ll=setTimeout('slide1.f1fff()',50);}else{lllllllllllll[ll].style.zIndex=2;lllllllllllll[lll].style.zIndex=3;f1ff();}}function f1ffff(){Beklemedemi=0;if(0==1){fffffffffffffff();}if(IEB){if(IEB==true){spageIcObj.filters[0].apply();}lllllllllllll[ll].style.visibility="hidden";lllllllllllll[lll].style.visibility="visible";if(IEB==true){spageIcObj.filters[0].play();}}else{op=1.00;lllllllllllll[ll].style.visibility="visible";lllllllllllll[lll].style.visibility="visible";lllllllllllll[ll].style.zIndex=3;lllllllllllll[lll].style.zIndex=2;lllllllllllll[ll].style.opacity = 1.00;lllllllllllll[lll].style.opacity = 1.00;f1fff();}}function f1fffffffffffffff(){Beklemedemi=0;if(0==1){fffffffffffffff();}lllll=lllll-Math.ceil(lllll/1.0);if(1==2){lllllllllllll[ll].style.left=""+(lllll-icwidth)+"px";lllllllllllll[lll].style.left=""+lllll+"px";}else if(1==3){lllllllllllll[ll].style.top=""+(lllll-icheight)+"px";lllllllllllll[lll].style.top=""+lllll+"px";}else if(1==4){lllllllllllll[ll].style.left=""+(icwidth-lllll)+"px";lllllllllllll[lll].style.left=""+lllll+"px";}else if(1==5){lllllllllllll[ll].style.top=""+(icheight-lllll)+"px";lllllllllllll[lll].style.top=""+lllll+"px";}if(lllll<=0){f1ffffffffffffff(trans_func_str);}else{ll1ll=setTimeout("slide1."+trans_func_str+"()",50);    }}function f1ffffffffffffff(fname){llll=lll;if(0==1){ffffffffffffff();}fffffffffffff();  Sonraki();if((1==0)||(1==1)){}else{if((1==3)||(1==5)){lllll=icheight;lllllllllllll[lll].style.top=""+lllll+"px";}else{lllll=icwidth;lllllllllllll[lll].style.left=""+lllll+"px"; }}if(1!=0){zindx();}Beklemedemi=1;if(llllllll==1){if(!((ReachedTheEnd==1)&&(1==0))){if(tam==0){if(ll1lllllll==0){devam(fname,5000);}}else{if(ll1lllllll==0){ll1ll=setTimeout('slide1.'+fname+'()',5000);}}}else{ll1ll=setTimeout('slide1.ff1ffff()',5000);}}}function ContinueSlideshow(){Beklemedemi=1;Sonraki();if((1==0)||(1==1)){}else{if((1==3)||(1==5)){lllll=icheight;lllllllllllll[lll].style.top=""+lllll+"px";}else{lllll=icwidth;lllllllllllll[lll].style.left=""+lllll+"px"; }}if((1!=0)&&(1!=1)){zindx();}if(llllllll==1){if(!((ReachedTheEnd==1)&&(1==0))){if(tam==0){if(ll1lllllll==0){devam("f1ffff",50);}}else{if(ll1lllllll==0){ll1ll=setTimeout("slide1."+"f1ffff"+"()",50);}} }else{ll1ll=setTimeout('slide1.ff1ffff()',50);}}}function Sonraki(){ll=lll;lll=lll+1;if(lll>=vnumbofimages){lll=0;ReachedTheEnd=1;}else{ReachedTheEnd=0;}      }function zindx(){lllllllllllll[lll].style.visibility="visible";lllllllllllll[ll].style.zIndex=2;lllllllllllll[lll].style.zIndex=3;    }function DoJustSwitch(){if(0==1){fffffffffffffff();}lllllllllllll[lll].style.visibility="visible";lllllllllllll[ll].style.visibility="hidden";f1ffffffffffffff(trans_func_str);}function f1f(c,n){ll=c;lll=n;if(1!=1){lllllllllllll[ll].style.visibility="visible";lllllllllllll[lll].style.visibility="visible";}lllllllllllll[ll].style.zIndex=2;lllllllllllll[lll].style.zIndex=3;}function ff1ffff(){if(ActionofLastImage==1){if(URLtoGo.length>3){if(URLtoGo.substring(0,11)=="javascript:"){eval(""+URLtoGo);return;}else{if(IEB){window.open(''+URLtoGo,'_self');}else{window.open(''+URLtoGo,'_self');}}}}}function devam(fm,pt){if((imgarr[lll].c==1)&&(imgarr[ll].c==1)){ll1ll=setTimeout('slide1.'+fm+'()',pt);}else{ll1ll=setTimeout('slide1.devam("'+fm+'",'+pt+')',5000);}var cagain=1;for(i=0;i<vnumbofimages;i++){if(imgarr[i].c==1){}else{cagain=0;}}if(cagain==1){tam=1;}}function ff1fffff(msec){if(tam==0){devam(trans_func_str,msec);}else{if(1==0){ll1ll=setTimeout("slide1.DoJustSwitch()",msec);}else if(1==1){ll1ll=setTimeout("slide1.f1ffff()",msec);}else{ll1ll=setTimeout("slide1.f1fffffffffffffff()",msec);}}}function ff1ffffff(){if(descgoster==1){ff1ff();}if(thumbgoster==1){CreateThumbnails();}for(i=0;i<3;i++){lllllllllllll[i]=document.getElementById("slide1d"+i);}if(0==1){f1fffffffffffff();}if(0==1){f1fffffffffff();}   if((0==1)||(0==1)||(0==1)||(0==1))   { f1ffffffffff();    }if(3>0){lllllllllllll[ll].style.visibility="visible";}if(3>1){ll=0;lll=0;llll=0;Sonraki();if(0==1){ffffffffffffff();}fffffffffffff();if(1==0){}else if(1==1){        }         else if(1==2)        {lllll=icwidth;lllllllllllll[lll].style.left=""+lllll+"px";zindx();                    }else if(1==3){lllll=icheight;lllllllllllll[lll].style.top=""+lllll+"px";zindx();            }          else if(1==4){lllll=icwidth;lllllllllllll[lll].style.left=""+lllll+"px";zindx();            }    else if(1==5){lllll=icheight;lllllllllllll[lll].style.top=""+lllll+"px";zindx();            }else if(1==6){zindx();}if(ll1lllllll==0){ff1fffff(5000);}}}function ff1fffffff(){spageObj = DisObj.appendChild(document.createElement("DIV"));spageObj.style.position="relative";spageObj.style.width=""+cwidth+"px";spageObj.style.height=""+cheight+"px";spageObj.style.overflow="hidden";spageObj.style.borderStyle=""+"solid";spageObj.style.borderWidth=""+0+"px";spageObj.style.borderColor="#"+"CCCCCC";if(0==1){spageObj.style.backgroundColor=""+"";}if(0==1){spageObj.style.backgroundImage="";spageObj.style.backgroundRepeat="repeat";spageObj.style.backgroundPosition=""+0+"px "+0+"px";}spageObj.style.visibility="inherit";if((0==1)||(0==1)||(0==1)||(0==1)){spageObj.onmouseover=ff1ffffffff;spageObj.onmouseout=ff1fffffffff;}spageIcObj = spageObj.appendChild(document.createElement("DIV"));spageIcObj.style.position="relative";spageIcObj.style.width=""+icwidth+"px";spageIcObj.style.height=""+icheight+"px";spageIcObj.style.overflow="hidden";spageIcObj.style.margin="0px";spageIcObj.style.padding="0px";spageIcObj.style.zIndex=(0+1);if(IEB==true){spageIcObj.style.filter='FILTER: progid:DXImageTransform.Microsoft.Fade(Overlap=1.00,duration=1.0,enabled=false);';spageIcObj.onfilterchange=f1ff;}dstr1='<div id="';dstr2='" style="position:absolute;visibility:hidden;left:0px;top:0px;padding:0px;margin:0px;overflow:hidden;">';dstr23='<table cellspacing="0" cellpadding="0" style="position:relative;left:0px;top:0px;padding:0px;margin:0px;"><tr><td style="width:'+icwidth+'px;height:'+icheight+'px;left:0px;top:0px;padding:0px;margin:0px;text-align:'+'center'+';vertical-align:'+'middle'+';">';dstr3='<img id="img';dstr4='" src="';dstr5='"  border="0" style="position:relative;'+'left:0px;top:0px;'+'border-style:'+'solid'+';border-width:'+'0'+'px;border-color:'+'#CCCCCC'+';'+""+'" alt=""></img>';dstr56='</td></tr></table>';dstr6='</div>';i=0;innertxt="";lnkstr="";lnkstrclose="";for(i=0;i<3;i++){if(linkstr[i].length>2){lnkstr='<a href="'+linkstr[i]+'" target="'+targetstr[i]+'" style="text-decoration: none;">';lnkstrclose="</a>";}else{lnkstr='';lnkstrclose='';}innertxt=innertxt+dstr1+"slide1d"+i+dstr2+dstr23+lnkstr+dstr3+i+dstr4+imgstr[i]+dstr5+lnkstrclose+dstr56+dstr6;}spageIcObj.innerHTML=""+innertxt;setTimeout('slide1.ff1ffffff()',100);}function Bas(){DisObj=document.getElementById('slide1sodis');if(!DisObj){setTimeout('slide1.Bas()',200);return;}        ff1fffffff();}var imgarr=new Array();var imgstr=new Array();var linkstr=new Array();var targetstr=new Array();var titarr=new Array();var desarr=new Array();var thumbimgstr=new Array();var thumbtextarr=new Array();imgarr[0]=new Image();imgarr[0].c=0;imgarr[0].onload = secondcomplate;imgarr[0].src=slide1foldername+'img/topimg1.jpg';imgstr[0]=slide1foldername+'img/topimg1.jpg';thumbimgstr[0]=slide1foldername+'img/topimg1.jpg';linkstr[0]='';targetstr[0]="";titarr[0]="";desarr[0]="";thumbtextarr[0]="";imgarr[1]=new Image();imgarr[1].c=0;imgarr[1].onload = secondcomplate;imgarr[1].src=slide1foldername+'img/topimg2.jpg';imgstr[1]=slide1foldername+'img/topimg2.jpg';thumbimgstr[1]=slide1foldername+'img/topimg2.jpg';linkstr[1]='';targetstr[1]="";titarr[1]="";desarr[1]="";thumbtextarr[1]="";imgarr[2]=new Image();imgarr[2].c=0;imgarr[2].onload = secondcomplate;imgarr[2].src=slide1foldername+'img/topimg.jpg';imgstr[2]=slide1foldername+'img/topimg.jpg';thumbimgstr[2]=slide1foldername+'img/topimg.jpg';linkstr[2]='';targetstr[2]="";titarr[2]="";desarr[2]="";thumbtextarr[2]="";var spageObj=null;var spageIcObj=null;var llllllllllllll = new Array();var lllllllllllllll = new Array();var ThumbContArr = new Array();var lll1llArr = new Array();var ThumbCurtainImgArr = new Array();var lllllllllllll = new Array();var lllllll=0;var llllllll=1;var descgoster=0;var thumbgoster=0;var ll1lllllll=0;var ActionofLastImage=0;var URLtoGo="";var moving_obj=null;var DisObj=null;var lll1llllllllll=null;var ll1ll=null;var ll=0;var lll=0;var llll=0;var Beklemedemi=1;var ReachedTheEnd=0;var llllll=0;var CY=0;var Art=0;var ll1l=null;var Tutyy=0;var Kalan=0;var TutFark=0;var ll1llllllllll=0;var ThumbScrollReason=0;var tam=0;var lllll=0;var ll1llllllllllll=null;var l1l=null;var l1ll=null;var l1lll=null;var l1llll=null;var IsManualAction=0;var DescCalcWidth=0;var ll1lllllllll=0;var DesAnimTimer=null;var ll1llllllll=2;var vnumbofimages=3;var varrrplc=null;var ll1lll=340;var ll1llll=340;var lllllllll=0;var llllllllll=0;var lllllllllll=0;var llllllllllll=0;var MoveIcWidth=0;var MoveIcHeight=0;var TBW=0;var thumbmouseic=0;var stepc=parseInt(20*(1.0));var dif=0.00;var op=1.00;dif=(1.00/stepc);var currstep=20;var stepcount=20;var ll1lllll=100;var ll1llllll=1.0;var BottomClip=0;var XStep=0;var YStep=0;var stepsay=0;var cwidth=0;var cheight=0;if((IEB==true)&&(scompat == "BackCompat" )){    cwidth=442;    cheight=365;}else{    cwidth=442-2*(0);    cheight=365-2*(0);}var icwidth=442-2*(0);var icheight=365-2*(0);var bgd=0;var trans_func_str="f1ffff";if(thumbgoster==1){this.CreateThumbnails=CreateThumbnails;this.ff=ff;this.fff=fff;this.ffff=ffff;this.fffff=fffff;this.ffffff=ffffff;this.fffffff=fffffff;this.ffffffff=ffffffff;this.fffffffff=fffffffff;this.ffffffffff=ffffffffff;this.fffffffffff=fffffffffff;this.ffffffffffff=ffffffffffff;this.fff1f=fff1f;this.fff1=fff1;this.thumbjustswitch=thumbjustswitch;}if(0==1){this.ff1ff=ff1ff;this.ffffffffffffff=ffffffffffffff;this.fffffffffffffff=fffffffffffffff;this.ffffffffffffffff=ffffffffffffffff;this.fffffffffffffffff=fffffffffffffffff;this.ff1f=ff1f;}if((0==1)||(0==1)||(0==1)||(0==1)){this.f1ffffff=f1ffffff;this.f1fffffff=f1fffffff;this.f1ffffffff=f1ffffffff;this.f1fffffffff=f1fffffffff;this.f1ffffffffff=f1ffffffffff;this.ff1ffffffff=ff1ffffffff;this.ff1fffffffff=ff1fffffffff;}this.fffffffffffff=fffffffffffff;this.ff1fff=ff1fff;this.f1f=f1f;this.f1ff=f1ff;this.f1fff=f1fff;this.f1ffff=f1ffff;this.f1fffff=f1fffff;this.f1fffffffffff=f1fffffffffff;this.f1ffffffffffff=f1ffffffffffff;this.f1fffffffffffff=f1fffffffffffff;this.devam=devam;this.f1ffffffffffffff=f1ffffffffffffff;this.Sonraki=Sonraki;this.zindx=zindx;this.f1fffffffffffffff=f1fffffffffffffff;this.ff1ffff=ff1ffff;this.ff1fffff=ff1fffff;this.ff1ffffff=ff1ffffff;this.ff1fffffff=ff1fffffff;this.Bas=Bas;this.DoJustSwitch=DoJustSwitch;this.ContinueSlideshow=ContinueSlideshow;}document.write("<div id=\"slide1sodis\"></div>");slide1=new slide1ScSlideShow();slide1.Bas();
+/*
+
+Better(?) Image cross fader (C)2004 Patrick H. Lauke aka redux
+
+Inspired by Steve at Slayeroffice http://slayeroffice.com/code/imageCrossFade/ 
+
+preInit "Scheduler" idea by Cameron Adams aka The Man in Blue
+http://www.themaninblue.com/writing/perspective/2004/09/29/ 
+
+Tweaked to deal with empty nodes 19 Feb 2006
+
+*/
+
+/* general variables */
+
+var galleryId = 'gallery'; /* change this to the ID of the gallery list */
+var	gallery; /* this will be the object reference to the list later on */
+var galleryImages; /* array that will hold all child elements of the list */
+var currentImage; /* keeps track of which image should currently be showing */
+var previousImage;
+var preInitTimer;
+preInit();
+
+/* functions */
+
+function preInit() {
+	/* an inspired kludge that - in most cases - manages to initially hide the image gallery list
+	   before even onload is triggered (at which point it's normally too late, and the whole list already
+	   appeared to the user before being remolded) */
+	if ((document.getElementById)&&(gallery=document.getElementById(galleryId))) {
+		gallery.style.visibility = "hidden";
+		if (typeof preInitTimer != 'undefined') clearTimeout(preInitTimer); /* thanks to Steve Clay http://mrclay.org/ for this small Opera fix */
+	} else {
+		preInitTimer = setTimeout("preInit()",2);
+	}
+}
+
+function fader(imageNumber,opacity) {
+	/* helper function to deal specifically with images and the cross-browser differences in opacity handling */
+	var obj=galleryImages[imageNumber];
+	if (obj.style) {
+		if (obj.style.MozOpacity!=null) {  
+			/* Mozilla's pre-CSS3 proprietary rule */
+			obj.style.MozOpacity = (opacity/100) - .001;
+		} else if (obj.style.opacity!=null) {
+			/* CSS3 compatible */
+			obj.style.opacity = (opacity/100) - .001;
+		} else if (obj.style.filter!=null) {
+			/* IE's proprietary filter */
+			obj.style.filter = "alpha(opacity="+opacity+")";
+		}
+	}
+}
+
+function fadeInit() {
+	if (document.getElementById) {
+		preInit(); /* shouldn't be necessary, but IE can sometimes get ahead of itself and trigger fadeInit first */
+		galleryImages = new Array;
+		var node = gallery.firstChild;
+		/* instead of using childNodes (which also gets empty nodes and messes up the script later)
+		we do it the old-fashioned way and loop through the first child and its siblings */
+		while (node) {
+			if (node.nodeType==1) {
+				galleryImages.push(node);
+			}
+			node = node.nextSibling;
+		}
+		for(i=0;i<galleryImages.length;i++) {
+			/* loop through all these child nodes and set up their styles */
+			galleryImages[i].style.position='absolute';
+			galleryImages[i].style.top=0;
+			galleryImages[i].style.zIndex=0;
+			/* set their opacity to transparent */
+			fader(i,0);
+		}
+		/* make the list visible again */
+		gallery.style.visibility = 'visible';
+		/* initialise a few parameters to get the cycle going */
+		currentImage=0;
+		previousImage=galleryImages.length-5;
+		opacity=100;
+		fader(currentImage,100);
+		/* start the whole crossfade process after a second's pause */
+		window.setTimeout("crossfade(100)", 1000);
+	}
+}
+
+function crossfade(opacity) {
+		if (opacity < 100) {
+			/* current image not faded up fully yet...so increase its opacity */
+			fader(currentImage,opacity);
+			/* fader(previousImage,100-opacity); */
+			opacity += 10;
+			window.setTimeout("crossfade("+opacity+")", 30);
+		} else {
+			/* make the previous image - which is now covered by the current one fully - transparent */
+			fader(previousImage,0);
+			/* current image is now previous image, as we advance in the list of images */
+			previousImage=currentImage;
+			currentImage+=1;
+			if (currentImage>=galleryImages.length) {
+				/* start over from first image if we cycled through all images in the list */
+				currentImage=0;
+			}
+			/* make sure the current image is on top of the previous one */
+			galleryImages[previousImage].style.zIndex = 0;
+			galleryImages[currentImage].style.zIndex = 100;
+			/* and start the crossfade after a second's pause */
+			opacity=0;
+			window.setTimeout("crossfade("+opacity+")", 10000);
+		}
+		
+}
+
+/* initialise fader by hiding image object first */
+addEvent(window,'load',fadeInit)
+
+
+
+/* 3rd party helper functions */
+
+/* addEvent handler for IE and other browsers */
+function addEvent(elm, evType, fn, useCapture) 
+// addEvent and removeEvent
+// cross-browser event handling for IE5+,  NS6 and Mozilla
+// By Scott Andrew
+{
+ if (elm.addEventListener){
+   elm.addEventListener(evType, fn, useCapture);
+   return true;
+ } else if (elm.attachEvent){
+   var r = elm.attachEvent("on"+evType, fn);
+   return r;
+ }
+} 
